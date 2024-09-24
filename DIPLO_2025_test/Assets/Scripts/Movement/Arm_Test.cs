528ea4c66a963d3b9e0691490e0cad725150e9ed
DenisRedaj <redaj.denis@gmail.com>
2024-09-25 00:31:44 +0200

    Collision physics incorporation prototype.

--- DIPLO_2025_test/Assets/Scripts/Movement/Arm_Test.cs
+++ DIPLO_2025_test/Assets/Scripts/Movement/Arm_Test.cs
@@ -10,11 +10,21 @@ public class Arm_Test : MonoBehaviour
     public TextMeshProUGUI button;
     public GameObject bone;
     public GameObject bone2;
+     public GameObject bone3;
     void Start(){}
 
     IEnumerator rotateJoint(GameObject bone, float angle, Vector3 axis, float time, float delay=0){
 
         yield return new WaitForSeconds(delay);
+        Coroutine routine = StartCoroutine(action(bone,angle,axis,time,delay));
+        yield return new WaitForSeconds(delay+time);
+        StopCoroutine(routine);
+        yield return null;
+    }
+
+    IEnumerator action(GameObject bone, float angle, Vector3 axis, float time, float delay=0){
+
+        
 
         Quaternion startRotation = bone.transform.rotation;
         float speed = angle/time;
@@ -33,6 +43,11 @@ public class Arm_Test : MonoBehaviour
         yield return new WaitForSeconds(.1f);
 
     }
+
+
+
+
+
         /*
         back	... Vector3(0, 0, -1).
         forward	... Vector3(0, 0, 1).
@@ -47,10 +62,24 @@ public class Arm_Test : MonoBehaviour
         one	    ... Vector3(1, 1, 1).
         */
 
+
+
+        /*
+        
+            NICO Bone Rotations
+
+        
+        
+        */
+
     public void clickTest(){
         Debug.Log ("button clicked" + bone.name + "transform: "+ bone.transform.rotation);
-        StartCoroutine(rotateJoint(bone, 45, Vector3.up, 3));
-        StartCoroutine(rotateJoint(bone2, 45, Vector3.up, 3, 4));
+        StartCoroutine(rotateJoint(bone2, 125, Vector3.right, 2));
+        StartCoroutine(rotateJoint(bone, 65, Vector3.up, 2, 3));
+        StartCoroutine(rotateJoint(bone2, 55, Vector3.left, 2, 5));     // to avoid jittering, don't use negative numbers ... flip vec3
+        StartCoroutine(rotateJoint(bone, 25, Vector3.down, 3, 7));
+        StartCoroutine(rotateJoint(bone3, 5, Vector3.up, 2, 9));
+        
 	}
 
 }
